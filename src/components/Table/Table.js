ac3f3f74aef680d3dce5a6abf1574087bfc1584c
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-09-28 15:57:30 +1000

    added the sort function for the table columns

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,22 +1,67 @@
 import React from "react";
 import "./Table.css";
-import employees from "../../utils/employees.json";
+// import employees from "../../utils/employees.json";
+
+const useSortableData = (employees, config = null) => {
+    const [sortConfig, setSortConfig] = React.useState(config);
+
+    const sortedItems = React.useMemo(() => {
+        let sortableItems = employees;
+        if (sortConfig !== null) {
+            sortableItems.sort((a, b) => {
+                if (a[sortConfig.key] < b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[sortConfig.key] > b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortableItems;
+    }, [employees, sortConfig]);
+
+    const requestSort = (key) => {
+        let direction = 'ascending';
+        if (
+            sortConfig &&
+            sortConfig.key === key &&
+            sortConfig.direction === 'ascending'
+        ) {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+    };
+
+    return { employees: sortedItems, requestSort, sortConfig };
+};
+
 
 
 const Table = props => {
+    const { employees, requestSort, sortConfig } = useSortableData(props.employees);
+    const getClassNamesFor = (name) => {
+        if (!sortConfig) {
+            return;
+        }
+        return sortConfig.key === name ? sortConfig.direction : undefined;
+    };
 
-    
     return (
         <div className="container">
             <table className="table">
                 <thead>
                     <tr>
                         {/* <th>id</th> */}
-                        <th>image</th>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>DOB</th>
+                        <th>Image</th>
+                        <th><button onClick={() => requestSort('name')}
+                            className={getClassNamesFor('name')}>Name</button></th>
+                        <th><button onClick={() => requestSort('phone')}
+                            className={getClassNamesFor('phone')}>Phone</button></th>
+                        <th><button onClick={() => requestSort('email')}
+                            className={getClassNamesFor('email')}>Email</button></th>
+                        <th><button onClick={() => requestSort('DOB')}
+                            className={getClassNamesFor('DOB')}>DOB</button></th>
                     </tr>
                 </thead>
                 <tbody>
