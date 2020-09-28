186ad435c32ad056e8e2f5536175dab555dcad29
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-09-28 21:59:31 +1000

    Updates

---
+++ precache-manifest.a430f9b095ecbe4c6e18212110d2a76e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f8441b2dfec4e39a8c4195f74e8249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c0f6b5a265866e6feb",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "21ed4622d386faf21321",
+    "url": "/static/css/main.6203cfef.chunk.css"
+  },
+  {
+    "revision": "b8c0f6b5a265866e6feb",
+    "url": "/static/js/2.6073fc02.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6073fc02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ed4622d386faf21321",
+    "url": "/static/js/main.6e8a5776.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
