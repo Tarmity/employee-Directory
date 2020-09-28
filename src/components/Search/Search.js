911d41ed2d1acf58263d2e4808aa40a6aa6d0025
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-09-28 12:54:52 +1000

    added the search function, unable to write an text into my search box

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,15 +2,19 @@ import React from "react";
 import './Search.css';
 import { Form, Button, FormControl } from 'react-bootstrap';
 
-const Search = () => {
+const Search = (props) => {
     return (
         <Form inline className="containers">
             <FormControl
                 className="search"
                 type="text" 
                 placeholder="Search.."
+                value= {props.search}
+                onChange= {props.handleInputChange}
+                 name= "term"
+                 id="term"
                 />
-                <Button type="button" className="btn btn-secondary">Submit</Button>               
+                {/* <Button type="button" className="btn btn-secondary">Submit</Button>                */}
         </Form>
 
     )
